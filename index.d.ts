088b6e14a2a7b0abac992e5e46834aa737aa1fea
diego-caceres-galvan <dcaceres@menlo-technologies.com>
2018-04-18 10:00:22 -0300

    feat: extended types definition

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,17 @@
+import { ViewProperties } from "react-native";
+
 declare module "react-native-sketch-draw" {
-  export interface SketchDraw {
-    selectedTool: number
-    toolColor: string
-    localSourceImagePath: string
-    toolThickness: number
-    constants: Constants
-  }
+  // export interface SketchDrawProps {
+  //   selectedTool: number
+  //   toolColor: string
+  //   localSourceImagePath: string
+  //   toolThickness: number
+  // }
+  export default class SketchDraw extends Component<SketchDrawProps & ViewProperties> {
+    static Constants: Constants;
+
+    onSaveSketch(imageResult: ImageResult): void;
+}
 
   export interface ImageResult {
     localFilePath: string
@@ -27,49 +33,7 @@ declare module "react-native-sketch-draw" {
 		name: string
   }
 
-  export function onSaveSketch(imageResult: ImageResult): void;
-
-  // export interface Options {
-  //     cropping?: boolean;
-  //     width?: number;
-  //     height?: number;
-  //     multiple?: boolean;
-  //     path?: string;
-  //     includeBase64?: boolean;
-  //     includeExif?: boolean;
-  //     cropperTintColor?: string;
-  //     cropperCircleOverlay?: boolean;
-  //     maxFiles?: number;
-  //     waitAnimationEnd?: boolean;
-  //     smartAlbums?: string[];
-  //     useFrontCamera?: boolean;
-  //     compressVideoPreset?: string;
-  //     compressImageMaxWidth?: number;
-  //     compressImageMaxHeight?: number;
-  //     compressImageQuality?: number;
-  //     loadingLabelText?: string;
-  //     mediaType?: string;
-  //     showsSelectedCount?: boolean;
-  //     showCropGuidelines?: boolean;
-  //     hideBottomControls?: boolean;
-  //     enableRotationGesture?: boolean;
-  // }
-
-  // export function openPicker(options: Options): Promise<Image | Image[]>;
-  // export function openCamera(options: Options): Promise<Image | Image[]>;
-  // export function openCropper(options: Options): Promise<Image>;
-  // export function clean(): Promise<void>;
-  // export function cleanSingle(path: string): Promise<void>;
-
-  // export interface ImageCropPicker {
-  //     openPicker(options: Options): Promise<Image | Image[]>;
-  //     openCamera(options: Options): Promise<Image | Image[]>;
-  //     openCropper(options: Options): Promise<Image>;
-  //     clean(): Promise<void>;
-  //     cleanSingle(path: string): Promise<void>;
-  // }
-
-  const SketchDraw: SketchDraw;
+  export function onSaveSketch(imageResult: ImageResult): void
 
   export default SketchDraw;
 }
