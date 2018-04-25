4bdd8262aa1d2398998a5a13a717581237c48ea3
diego-caceres-galvan <dcaceres@menlo-technologies.com>
2018-04-25 10:56:06 -0300

    fix in ios

--- ios/SketchView/SketchView.m
+++ ios/SketchView/SketchView.m
@@ -81,7 +81,8 @@
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
-    UIImage * scaled = [self scaleImage: incrementalImage toSize:rect]
+    CGSize size = CGSizeMake(rect.size.width, rect.size.height);
+    UIImage * scaled = [self scaleImage: incrementalImage toSize:size];
     // Drawing code
     [scaled drawInRect:rect];
     [currentTool render];
